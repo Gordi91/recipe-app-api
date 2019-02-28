5ba517dce9af012173d7a3181356dc4a180c1ad4
Gordi91 <szymon.pecuch@hotmail.com>
2019-02-28 21:42:10 +0100

    Assigned only tags filter implemented

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -18,7 +18,12 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(self.request.query_params.get('assigned_only'))
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
         """Create a new ingredient"""
@@ -47,7 +52,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def _params_to_ints(self, qs):
         """Converts a list of string IDs to a list of integers"""
         return [int(str_id) for str_id in qs.split(',')]
-
 
     def get_queryset(self):
         """Retrive the recipes for the authenticated user"""
